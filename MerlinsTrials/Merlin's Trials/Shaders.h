3d029c89f4f4e4d998db1144851e255d0ed6ebd5
sebbedelius <sebbe_fj@hotmail.com>
2018-04-23 12:37:06 +0200

    Shaders borde vara klar

--- MerlinsTrials/Merlin's Trials/Shaders.h
+++ MerlinsTrials/Merlin's Trials/Shaders.h
@@ -10,48 +10,47 @@
 #include <wrl\client.h>
 #include <vector>
 #include <SimpleMath.h>
+
+enum SHADER {
+	VertexColor,
+	VertexNormal,
+	VertexNormalTexture
+};
+
 class Shaders
 {
 private:
-	enum SHADER { Basic, Glowing }; //Till exempel
+	
 	ID3D11Device* gDevice;
 	ID3D11DeviceContext* gDeviceContext;
 
-	DirectX::XMMATRIX wMatrix;	
-	DirectX::XMMATRIX proj;
-	DirectX::XMMATRIX view;
-	DirectX::SimpleMath::Vector3 specColor;
-	DirectX::SimpleMath::Vector3 diffuseColor;
-	DirectX::XMVECTOR camPos;
-
-	float specExponent;
-	float opacity;
-
-	DirectX::XMVECTOR lightDirection;
-	DirectX::XMVECTOR lightDiffuseColor;
-	DirectX::XMVECTOR lightSpecularColor;
-
 	std::unique_ptr<DirectX::BasicEffect> m_effect;	
-	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout; //Eller en egen input layout	
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout; 	
 
 	ID3D11Buffer* vBuffer;
 	ID3D11Buffer* iBuffer;
 
-	void createBasicShader();
+	void createVertexPositionColorShader();
+	void createVertexPositionNormalShader();
+	void createVertexPositionNormalTextureShader();
 
 public:	
-	Shaders(ID3D11Device* in_gDevice, ID3D11DeviceContext* in_gDeviceContext); //Inkluderar 
+	Shaders(ID3D11Device* in_gDevice, ID3D11DeviceContext* in_gDeviceContext, ID3D11Buffer* in_vBuffer, ID3D11Buffer* in_iBuffer); 
 	~Shaders();
 
 	void createShader(SHADER whatShader);
 	void setShaderParams(DirectX::XMMATRIX in_WMatrix, DirectX::SimpleMath::Vector3 in_SpecColor, DirectX::SimpleMath::Vector3 in_DiffuseColor,
 		DirectX::SimpleMath::Vector3 in_Emissive, float in_SpecExponent, float in_Opacity);
 	void setCameraParams(DirectX::XMMATRIX in_Proj, DirectX::XMMATRIX in_View);
-	void setShaderLightParams(DirectX::SimpleMath::Vector3 in_Ambient, DirectX::XMVECTOR in_IightDirection[DirectX::IEffectLights::MaxDirectionalLights], DirectX::XMVECTOR in_LightDiffuseColor[DirectX::IEffectLights::MaxDirectionalLights],
-		DirectX::XMVECTOR in_LightSpecularColor[DirectX::IEffectLights::MaxDirectionalLights]);
-	void setShaderFogParams(DirectX::XMVECTOR in_FogColor, DirectX::XMVECTOR fogVector);
-	void setTexture(Texture in_Tex);
-
+	void setShaderLightParams(DirectX::SimpleMath::Vector3 in_Ambient, int in_WhichLight, DirectX::XMVECTOR in_LightDirection,
+		DirectX::XMVECTOR in_LightDiffuseColor, DirectX::XMVECTOR in_LightSpecularColor);
+	void setShaderFogParams(DirectX::XMVECTOR in_FogColor);
+	void setTexture(ID3D11ShaderResourceView*  in_Tex);
+	//void disableSpecular();
+	void setAlpha(float in_Alpha);
+	void setLightingEnabled(bool in_Value);
+	void setSpecificLightEnabled(int in_WhichLight, bool in_Value);
+	void setLightDirection(int in_WhichLight, DirectX::XMVECTOR in_Direction);
 
 };
 
