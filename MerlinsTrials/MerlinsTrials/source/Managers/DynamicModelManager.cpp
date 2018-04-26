a3e1bb1e7b36964612bd7ffbb05596890983fb8e
Jonatan Fransson <jonte.frasse@gmail.com>
2018-04-26 14:53:26 +0200

    wut?

--- MerlinsTrials/MerlinsTrials/source/Managers/DynamicModelManager.cpp
+++ MerlinsTrials/MerlinsTrials/source/Managers/DynamicModelManager.cpp
@@ -11,8 +11,6 @@ DynamicModelManager::~DynamicModelManager()
 
 void DynamicModelManager::initialize(std::vector<Geometry*> in_geometry, Player* in_player, ID3D11Device* in_device, ID3D11DeviceContext* in_context)
 {
-
-
 }
 
 void DynamicModelManager::update(const Dimension & in_dim)
